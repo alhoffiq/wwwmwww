4ca02a8b5d0dff1c0bc6572335c7d6b76ce90fb3
alhoffiq <andrew.hoff14@yahoo.com>
2021-05-17 21:14:20 -0700

    Another fix for ghpages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import { Link, useLocation } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavbarComp = () => {
+    const location = useLocation();
+
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>wwwmwww</Navbar.Brand>
+            <Navbar.Brand>Andrew Hoff</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/puzzles">Puzzles</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
+                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
+                <Link to="/puzzles" className={location.pathname === "/puzzles" ? "nav-link active" : "nav-link"}>Puzzles</Link>
+                <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
             </Nav>
         </Navbar>
     );
