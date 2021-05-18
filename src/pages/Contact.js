841e690e63cee6cce8dc5b30c62a777554a2fdb1
alhoffiq <andrew.hoff14@yahoo.com>
2021-05-17 20:15:06 -0700

    Added placeholder contacts

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,24 @@
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <section className="container rounded">
+            <div className="row">
+                <div className="col">
+                    <h2 className="header text-center">Contacts</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 mx-auto">
+                    <div className="card">
+                        <div className="list-group">
+                            <a href="mailto:carl.n.hoff@gmail.com" className="list-group-item list-group-item-action text-center" target="_blank" rel="noreferrer">Email</a>
+                            <a href="#" className="list-group-item list-group-item-action text-center" target="_blank" rel="noreferrer">Twisty Puzzles</a>
+                            <a href="#" className="list-group-item list-group-item-action text-center" target="_blank" rel="noreferrer">Shapeways</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
